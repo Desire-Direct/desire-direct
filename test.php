6956640fbcf3c59e0def778b648e2f0e933c6604
Verena Hermann <verenahermann@gmail.com>
2026-05-06 17:19:37 +0200

    add moorhead

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html> 
+<body>
+
+
+<?php 
+/* multi-line comment */
+echo 'Hello World!<br>';
+echo phpversion();
+?>
+
+<p>
+installed php, php-apache, php-imagick;
+edit /etc/httpd/conf/httpd.conf according to https://wiki.archlinux.org/title/Apache_HTTP_Server#PHP; maybe need librsvg dependency for svg support?
+
+</p>
+
+</body> 
+</html>
